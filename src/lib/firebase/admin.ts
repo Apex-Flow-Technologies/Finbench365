45642562af05973b2401cc3e667acdf446514f1b
Apex-Flow-Technologies <harisudhan@apexflowtechnologies.com>
2026-07-21 21:03:13 +0530

    fix: add dummy fallbacks for vercel static build

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -8,9 +8,9 @@ if (!getApps().length) {
   try {
     app = initializeApp({
       credential: cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'dummy-project-id',
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL || 'dummy@dummy.com',
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY || '-----BEGIN PRIVATE KEY-----\ndummy\n-----END PRIVATE KEY-----\n').replace(/\\n/g, '\n'),
       }),
     });
   } catch (error: any) {
